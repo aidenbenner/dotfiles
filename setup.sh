4b28bfef12dc9b76216d961b6db592e0857d648a
aidenbenner <aiden.benner@gmail.com>
2017-06-03 23:25:45 -0400

    separate link

--- setup.sh
+++ setup.sh
@@ -42,40 +42,7 @@ mkdir -p ~/.vim/colors
 echo Install Antigen
 git clone https://github.com/zsh-users/antigen ~/.antigen/antigen 
 
-echo Linking files
-
-rm ~/.vimrc_old
-mv ~/.vimrc ~/.vimrc_old
-ln -sfv $PWD/vimrc ~/.vimrc
-
-rm ~/.vim/templates/comp.old
-mv ~/.vim/templates/comp.cc ~/.vim/templates/comp.old
-ln -sfv $PWD/comp.cc ~/.vim/templates/comp.cc
-
-rm ~/.gitconfig_old
-mv ~/.gitconfig ~/.gitconfig_old
-ln -sfv $PWD/gitconfig ~/.gitconfig
-
-rm ~/.tmux_conf.old
-mv ~/.tmux.conf ~/.tmux_conf.old
-ln -sfv $PWD/tmux.conf ~/.tmux.conf
-
-rm ~/.bash_aliases.old
-mv ~/.bash_aliases ~/.bash_aliases.old
-ln -sfv $PWD/bash_aliases ~/.bash_aliases
-
-rm ~/.bashrc.old
-mv ~/.bashrc ~/.bashrc.old
-ln -sfv $PWD/bashrc ~/.bashrc
-
-rm ~/.zshrc.old
-mv ~/.zshrc ~/.zshrc.old
-ln -sfv $PWD/zshrc ~/.zshrc
-ln -sfv $PWD/ycm_extra_conf.py ~/.ycm_extra_conf.py
-
-ln -sfv $PWD/gitignore_global ~/.gitignore_global
-
-
+sh link.sh 
 sudo chown -R $USER: ~/.vim/
 
 vim +PlugInstall +qall
